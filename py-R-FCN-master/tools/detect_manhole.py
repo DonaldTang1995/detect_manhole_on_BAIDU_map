5ab755ee6fa46f1adaad336c6879daeaa4a9ac50
tangshitao <zy15779@nottingham.edu.cn>
2017-01-24 16:50:43 +0800

    modify detect_manhole

--- py-R-FCN-master/tools/detect_manhole.py
+++ py-R-FCN-master/tools/detect_manhole.py
@@ -17,29 +17,29 @@ NETS={"resnet50_rfcn_class_12_iter_100000":("resnet50_rfcn_class_12_iter_100000.
 
 CONF_THRESH = 0.8
 NMS_THRESH = 0.3
+def detect(img,net_name="resnet50_rfcn_class_12_iter_100000",mode='gpu',gpu_id=0):
+    cfg.TEST.HAS_RPN = True
 
-def detect(img,net_name="class_12_iter_100000",mode='gpu',gpu_id=0):
-	prototxt_name,model_name=NETS[net_name]
-	prototxt=os.path.join(manhole_net_path,prototxt_path,prototxt_name)
-	model=os.path.join(manhole_net_path,model_path,model_name)
+    prototxt_name,model_name=NETS[net_name]
+    prototxt=os.path.join(cfg.ROOT_DIR,manhole_net_path,prototxt_path,prototxt_name)
+    model=os.path.join(cfg.ROOT_DIR,manhole_net_path,model_path,model_name)
 
-	if not os.path.isfile(model):
+    if not os.path.isfile(model):
         raise IOError(('{:s} not found.\n').format(model))
 
     if not os.path.isfile(prototxt):
-    	raise IOError(('{:s} not found.\n').format(prototxt))
+        raise IOError(('{:s} not found.\n').format(prototxt))
 
     if mode=="gpu":
-    	caffe.set_mode_gpu()
-        caffe.set_device(args.gpu_id)
-        cfg.GPU_ID = args.gpu_id
+        caffe.set_mode_gpu()
+        caffe.set_device(gpu_id)
+        cfg.GPU_ID = gpu_id
     elif mode=="cpu":
-    	caffe.set_mode_cpu()
+        caffe.set_mode_cpu()
 
-    net = caffe.Net(prototxt, caffemodel, caffe.TEST)
+    net = caffe.Net(prototxt, model, caffe.TEST)
 
-    print '\n\nLoaded network {:s}'.format(caffemodel)
+    print '\n\nLoaded network {:s}'.format(model)
 
-    scores, boxes = im_detect(net, im)
-    inds = np.where(scores>CONF_THRESH)[0]
-    return scores[inds],boxes[inds]
+    return im_detect(net, img)
+

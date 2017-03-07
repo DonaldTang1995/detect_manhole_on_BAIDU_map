47d77b27eacea3c4173547c373d764d74aff718f
team201603 <team201603@cs-linux.nottingham.edu.cn>
2017-03-07 10:49:22 +0800

    add html

--- main_server/main.py
+++ main_server/main.py
@@ -4,9 +4,14 @@ import __init__
 
 from ucs_server import UCS
 from index import index
+from baidu import baidu
+from baidu2 import baidu
 urls = (
     '/UCS', 'UCS',
-    '/index','index'
+    '/index','index',
+    '/baidu','baidu',
+    '/sadas','baidu2',
+
 )
 if __name__ == "__main__": 
     web.config.debug=False

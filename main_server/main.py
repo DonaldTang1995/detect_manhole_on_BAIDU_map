2aa6aaa00aba42a77c978ecd46580e8c90bbf2bf
team201603 <team201603@cs-linux.nottingham.edu.cn>
2017-03-10 11:00:56 +0800

    add utf8 support

--- main_server/main.py
+++ main_server/main.py
@@ -1,6 +1,7 @@
 import web
 import logging
 import __init__
+import sys
 
 from ucs_server import UCS
 from index import index
@@ -12,6 +13,8 @@ urls = (
 
 )
 if __name__ == "__main__": 
+    reload(sys)
+    sys.setdefaultencoding('utf8')
     web.config.debug=False
     logging.info("UCS starting...")
     app = web.application(urls, globals())
